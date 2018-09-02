ec63bf5ea517ac973b4e1e7b0cb11ea27110ed00
batchen rothenberg <batcheni89@gmail.com>
2018-09-02 15:06:16 +0300

    added script to run TCAS benchmarks as in the FM paper

---
+++ scripts/RunTcas.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+source ./AllRepair.sh tcas -m 1 -s 2 --repair-out --no-mut Inhibit_Biased_Climb_correct,Non_Crossing_Biased_Climb_correct,Non_Crossing_Biased_Descend_correct,Own_Below_Threat_correct,Own_Above_Threat_correct,alt_sep_test_correct
