2b3129ee47bee6cc7ac6955f3e77bbc4133ffceb
batchen rothenberg <batcheni89@gmail.com>
2018-09-05 01:53:50 +0300

    added option to optimize or and and expressions in slicing

--- python/batSlicing.py
+++ python/batSlicing.py
@@ -3,6 +3,7 @@ from z3 import *
 #from z3util import get_vars
 
 _debug_slicing = False
+_simplification_flag = True
 
 def slice_program(sat_solution, smt_solution, assert_and_assume_constraints, constraints, assigning_constraint_map, soft_constraints):
     demands = [constraints[cons_i] for cons_i in assert_and_assume_constraints]
@@ -13,7 +14,10 @@ def slice_program(sat_solution, smt_solution, assert_and_assume_constraints, con
         print [smt_solution.evaluate(simpl_demand) for simpl_demand in simpl_demands]
     # START SLICING
     relevant_constraints = set(assert_and_assume_constraints)
-    relevant_vars = [var.__str__() for var in get_vars(Or(simpl_demands))]
+    if _simplification_flag:
+        relevant_vars = [var.__str__() for var in get_impacting_vars(And(demands),smt_solution)]
+    else:
+        relevant_vars = [var.__str__() for var in get_vars(Or(simpl_demands))]
     # TODO: optimization: move calculation up to here to be done once in init, without simplification
     unproccesed_vars_stack = list(relevant_vars)
     while unproccesed_vars_stack:
@@ -34,14 +38,30 @@ def slice_program(sat_solution, smt_solution, assert_and_assume_constraints, con
             if rhs.decl().__str__()=="If": # phi function assignment
                 # Add guard variables anyway
                 guard = rhs.arg(0)
-                new_vars = get_vars(find_satisfied_subexpression(guard, smt_solution))
+                if _simplification_flag:
+                    new_vars = get_impacting_vars(guard,smt_solution)
+                else:
+                    new_vars = get_vars(find_satisfied_subexpression(guard, smt_solution))
                 # Add variables of relevant branch only
                 if is_true(smt_solution.evaluate(guard)):
-                    new_vars.extend(get_vars(rhs.arg(1)))
+                    if _debug_slicing:
+                        print guard, " is true"
+                    if _simplification_flag:
+                        new_vars.extend(get_impacting_vars(rhs.arg(1),smt_solution))
+                    else:
+                        new_vars.extend(get_vars(rhs.arg(1),smt_solution))
                 else:
-                    new_vars.extend(get_vars(rhs.arg(2)))
+                    if _debug_slicing:
+                        print guard, " is false"
+                    if _simplification_flag:
+                        new_vars.extend(get_impacting_vars(rhs.arg(2),smt_solution))
+                    else:
+                        new_vars.extend(get_vars(rhs.arg(2),smt_solution))
             else: # standard assignment
-                new_vars = get_vars(find_satisfied_subexpression(rhs, smt_solution))
+                if _simplification_flag:
+                    new_vars = get_impacting_vars(rhs,smt_solution)
+                else:
+                    new_vars = get_vars(find_satisfied_subexpression(rhs, smt_solution))
             # Add new_vars to stuck, only if never added before
             for new_var in new_vars:
                 if not new_var.__str__() in relevant_vars:
@@ -88,6 +108,34 @@ def find_satisfied_subexpression(expr, smt_solution):
                 print "breaking"
                 break
     return expr
+
+
+def get_impacting_vars(f, smt_solution, rs=[]):
+    if __debug__:
+        assert is_expr(f)
+    # base cases
+    if is_const(f):
+        if is_expr_val(f):
+            return rs
+        else:  #variable
+            return vset(rs + [f],str)
+    # recursive calls
+    else:
+        #print "else"
+        if is_and(f) and is_false(smt_solution.evaluate(f)):
+            for arg in f.children():
+                if is_false(smt_solution.evaluate(arg)):
+                    rs = get_impacting_vars(arg,smt_solution,rs)
+                    break
+        elif is_or(f) and is_true(smt_solution.evaluate(f)):
+            for arg in f.children():
+                if is_true(smt_solution.evaluate(arg)):
+                    rs = get_impacting_vars(arg,smt_solution,rs)
+                    break
+        else:
+            for arg in f.children():
+                rs = get_impacting_vars(arg, smt_solution, rs)
+        return vset(rs, str)
 
 
 ######################################################################################################
