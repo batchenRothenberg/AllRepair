632c0087edc6f2d014206c4f84fcc05589c6b06f
batchen <batcheni89@gmail.com>
2019-01-11 01:32:24 +0200

    Pretty printing: variables have their original names

--- python/batRepairPrinter.py
+++ python/batRepairPrinter.py
@@ -1,18 +1,30 @@
 from z3 import *
 from z3printer import *
+from batutils import find_regular_expression
 
 
 def pretty_print_repair_expression(e):
     out = io.StringIO()
-    PP()(out, Formatter()(e))
+    # PP()(out, Formatter()(e)) # applying () to a class object calls the __call__ method of the class
+    PP()(out, RepairFormatter()(e)) # applying () to a class object calls the __call__ method of the class
     return out.getvalue()
-    #return "pretty repair: " + obj_to_string(e)
-    #oldFormater = _Formatter
-    #_Formatter = RepairFormatter()
-    #print e
-    #_Formatter = oldFormater
 
 
-class RepairFormatter(Formatter):
-    def __init__(self, e):
-        super.__init__(e)
+class RepairFormatter(object, Formatter):
+
+    def __init__(self):
+        Formatter.__init__(self)
+
+    def pp_const(self, a):
+        name = a.decl().name()
+        res = find_regular_expression('::([^:]*)!',name)
+        if res:
+            pretty_name = res[0]
+        else:
+            new_res = find_regular_expression(':([^:]*)#', name)
+            pretty_name = new_res[0]
+        return to_format(pretty_name)
+
+
+
+
