7c8f11f323f246f2953c2879dfb24dca00782f63
batchen <batcheni89@gmail.com>
2019-01-13 01:30:22 +0200

    pretty printing: do not print 'guard=..'

--- python/batRepairPrinter.py
+++ python/batRepairPrinter.py
@@ -4,9 +4,19 @@ from batutils import find_regular_expression
 
 
 def pretty_print_repair_expression(e):
+    assert is_eq(e)
     out = io.StringIO()
-    # PP()(out, Formatter()(e)) # applying () to a class object calls the __call__ method of the class
-    PP()(out, RepairFormatter()(e)) # applying () to a class object calls the __call__ method of the class
+    # If e assigns to auxiliary guard - remove assignment
+    lhs = e.arg(0)
+    rhs = e.arg(1)
+    name = lhs.decl().name()
+    is_name_a_guard = find_regular_expression('goto_symex::.*guard', name)
+    if is_name_a_guard:
+        # PP()(out, Formatter()(rhs)) # Uncomment to disable pretty printing (e.g., for debugging)
+        PP()(out, RepairFormatter()(rhs))  # applying () to a class object calls the __call__ method of the class
+    else:
+        # PP()(out, Formatter()(e)) # Uncomment to disable pretty printing (e.g., for debugging)
+        PP()(out, RepairFormatter()(e)) # applying () to a class object calls the __call__ method of the class
     return out.getvalue()
 
 
