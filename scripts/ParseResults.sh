110070f27f9b6b3753a9dc5cc8f385af4b2fdedb
batchen <batcheni89@gmail.com>
2019-01-23 17:27:46 +0200

    ParseRersults: uses associative arrays

--- scripts/ParseResults.sh
+++ scripts/ParseResults.sh
@@ -2,49 +2,79 @@
 
 results_dir="AllRepairResults"
 separating_string="___________________"
-# Order in column_titles and column_identifying_texts must be the same, and determines column order in output.
-declare -a column_titles=( "file name" "hard constraints" "max mutation size" "mutated programs" )
-declare -a column_identifying_texts=( "Repairing file " "Hard constraints (group 0): " "Max mutation size: " "Mutated programs in search space: ")
-declare -a current_row
+declare -a column_keys=( "filename" "hard" "soft" "programs" "translation" "repair" "numrepairs")
+declare -A column_titles=( ["filename"]="file name" ["hard"]="hard constraints" ["soft"]="max mutation size" ["programs"]="mutated programs" ["translation"]="translation result" ["repair"]="repair result" ["numrepairs"]="found repairs" )
+declare -A prefix_strings=( ["filename"]="Repairing file " ["hard"]="Hard constraints (group 0): " ["soft"]="Max mutation size: " ["programs"]="Mutated programs in search space: " )
+declare -A current_row
 
-# Determine output file
-if [ -z "$1" ]; then
-	if ! [ -d "$results_dir" ]; then
-		mkdir "$results_dir"
-	fi
-	results_filename="$results_dir/AllRepair_results_"`date +'%d_%m_%Y_%X'`
-else
-	results_filename="$1"
-fi
-
-# Print date and time (overwrites existing content!)
-echo `date +'%d_%m_%Y_%X'` > $results_filename
-
-# Print title 
-for column in "${column_titles[@]}"; do 
-	echo -n "${column}," >> $results_filename
-done
-echo "" >> $results_filename
-
-# Parse input stream (from AllRepair) and add to output
-while read line; do
-	if [[ $line == "${separating_string}"* ]]; then
-		# Finished repairing file - print current_row
-		for i in "${!column_titles[@]}"; do 
-            		echo -n "${current_row[$i]}," >> $results_filename
-        	done
-		echo "" >> $results_filename
-		# Initialize current_row array
-		unset current_row
-	fi
-	for index in "${!column_identifying_texts[@]}"; do 
-		identifying_string=${column_identifying_texts[$index]}
-		if [[ $line == "$identifying_string"* ]]; then
-			info=${line#"$identifying_string"}
-			current_row[$index]=$info
+main() {
+	# Determine output file
+	if [ -z "$1" ]; then
+		if ! [ -d "$results_dir" ]; then
+			mkdir "$results_dir"
 		fi
-	done
-	echo $line
-done
+		results_filename="$results_dir/AllRepair_results_"`date +'%d_%m_%Y_%X'`
+	else
+		results_filename="$1"
+	fi
 
+	# Print date and time (overwrites existing content!)
+	echo `date +'%d_%m_%Y_%X'` > $results_filename
+
+	# Print title 
+	for key in "${column_keys[@]}"; do 
+		echo -n "${column_titles[$key]}," >> $results_filename
+	done
+	echo "" >> $results_filename
+
+	# Parse input stream (from AllRepair) and add to output
+	while read line; do
+		if [[ $line == "${separating_string}"* ]]; then
+			# Finished repairing file - print current_row
+			for key in "${column_keys[@]}"; do 
+		    		echo -n "${current_row[$key]}," >> $results_filename
+			done
+			echo "" >> $results_filename
+			# Initialize current_row array
+			unset current_row
+			declare -A current_row
+		fi
+		check_prefix_strings "$line"
+		check_translation "$line"
+		check_repair "$line"
+		echo $line
+	done
+}
+
+check_repair () {
+	if [[ $1 == "AllRepair: REPAIR PROCESS TERMINATED SUCCESSFULLY" ]]; then
+		current_row["repair"]="SUCCESS"
+	elif [[ $1 == "AllRepair: ERROR DURING REPAIR" ]]; then
+		current_row["repair"]="FAILURE"
+	fi
+}
+
+check_translation () {
+	if [[ $1 == "AllRepair: TRANSLATION PROCESS TERMINATED SUCCESSFULLY" ]]; then
+		current_row["translation"]="SUCCESS"
+	elif [[ $1 == "AllRepair: ERROR DURING TRANSLATION" ]]; then
+		current_row["translation"]="FAILURE"
+	fi
+}
+
+check_prefix_strings () {
+	for key in "${!prefix_strings[@]}"; do 
+		identifying_string=${prefix_strings[$key]}
+		check_prefix_string_and_save "$1" "${identifying_string}" "${key}"
+	done
+}
+
+check_prefix_string_and_save () {
+	if [[ $1 == "$2"* ]]; then
+		info=${1#"$2"}
+		current_row[$3]=$info
+	fi
+}
+
+main "$@"
 
