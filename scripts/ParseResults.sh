ec6e6ebeb19a8d9276018cbe8a3cd1b638190c7b
batchen <batcheni89@gmail.com>
2019-02-02 20:31:56 +0200

    ParseResults: bugfix: print multiline repair by using repair scope

--- scripts/ParseResults.sh
+++ scripts/ParseResults.sh
@@ -6,6 +6,7 @@ declare -a column_keys=( "filename" "hard" "soft" "programs" "translation" "repa
 declare -A column_titles=( ["filename"]="file name" ["hard"]="hard constraints" ["soft"]="max mutation size" ["programs"]="mutated programs" ["translation"]="translation result" ["repair"]="repair result" ["numrepairs"]="found repairs" ["timetofirst"]="time to first repair [s]" ["maxinspected"]="max inspected size" ["satchecktotal"]="sat check [s]" ["satblocktotal"]="sat block [s]" ["smttotal"]="smt check [s]" ["setuptotal"]="setup [s]" ["smtcount"]="smt check count" ["total"]="total repair time [s]" ["translationtime"]="translation time [s]" ["totaltofirst"]="total time to first repair" )
 declare -A prefix_strings=( ["filename"]="Repairing file " ["hard"]="Hard constraints (group 0): " ["soft"]="Max mutation size: " ["programs"]="Mutated programs in search space: " ["maxinspected"]="Max inspected size: " ["satchecktotal"]="SAT check : " ["satblocktotal"]="SAT block : " ["smttotal"]="SMT check : " ["setuptotal"]="setup : " ["total"]="total : " ["smtcount"]="SMT check count :" ["translationtime"]="AllRepair: Translation duration: " )
 declare -A current_row
+in_repair_scope=0 # boolean
 
 main() {
 	# Determine output files: $1 for results table, $2 for list of found repairs
@@ -59,9 +60,10 @@ main() {
 		check_prefix_strings "$line"
 		check_translation_result "$line"
 		check_repair_result "$line"
-		check_repair_found "$line"
-		save_repair "$line"
-		check_time_to_first_repair "$line"
+		update_repair_scope_and_separate_files "$line"
+		update_repair_count "$line"
+		save_repair "$line" # must come after update_repair_scope_and_separate_files
+		check_time_to_first_repair "$line" # must come after update_repair_count
 		echo $line
 	done
 	
@@ -70,8 +72,18 @@ main() {
 	echo `date +'%d_%m_%Y_%X'` >> $repairs_filename
 }
 
+update_repair_scope_and_separate_files () {
+	if [[ "$1" == "-------------"* ]]; then
+		# if first repair- print line to separate this file for previous files
+		if [ ${current_row["numrepairs"]} -eq 0 ]; then
+			echo "__________________________________________________________________________" >> $repairs_filename
+		fi 
+		in_repair_scope=$((1-in_repair_scope))
+	fi
+}
+
 save_repair () {
-	if [[ "$1" == "Elapsed time: "* ]] || [[ "$1" == "In file "* ]] || [[ "$1" == "Replace "* ]]; then
+	if ((in_repair_scope)); then
 		echo "$1" >> $repairs_filename	
 	fi
 }
@@ -88,13 +100,9 @@ check_time_to_first_repair () {
 	fi
 }
 
-check_repair_found () {
-	if [[ $1 == "Possible solution:" ]]; then
+update_repair_count () {
+	if [[ $1 == "Possible repair:" ]]; then
 		let current_row["numrepairs"]+=1
-		# if first repair- print line to separate this file for previous files
-		if [ ${current_row["numrepairs"]} -eq 1 ]; then
-			echo "_________________________________________________" >> $repairs_filename
-		fi
 	fi
 }
 
