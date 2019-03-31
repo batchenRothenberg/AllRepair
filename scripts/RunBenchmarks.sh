a494f91ee78488e9379d2d7ef68905435ba428b0
batchen <batcheni89@gmail.com>
2019-03-31 12:19:27 +0300

    RunBenchmarks: saves to directories TCAS and QLOSE

--- scripts/RunBenchmarks.sh
+++ scripts/RunBenchmarks.sh
@@ -2,30 +2,30 @@
 
 # Reproduce TCAS experiments
 # mutation level 1
-./AllRepair.sh Benchmarks/Tcas -m 1 -t 600 -s 2 2>&1 | ./ParseResults.sh
+./AllRepair.sh Benchmarks/Tcas -m 1 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/TCAS
 # mutation level 2
-./AllRepair.sh Benchmarks/Tcas -m 2 -t 600 -s 2 2>&1 | ./ParseResults.sh
+./AllRepair.sh Benchmarks/Tcas -m 2 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/TCAS
 
 # Reroduce Qlose experiments
 # mutation level 1, increasing bounds 
-./AllRepair.sh Benchmarks/Qlose/loops -m 1 -u 1 -t 600 -s 2 2>&1 | ./ParseResults.sh
-./AllRepair.sh Benchmarks/Qlose/loops -m 1 -u 2 -t 600 -s 2 2>&1 | ./ParseResults.sh
-./AllRepair.sh Benchmarks/Qlose/loops -m 1 -u 5 -t 600 -s 2 2>&1 | ./ParseResults.sh
-./AllRepair.sh Benchmarks/Qlose/loops -m 1 -u 10 -t 600 -s 2 2>&1 | ./ParseResults.sh
-./AllRepair.sh Benchmarks/Qlose/loops -m 1 -u 15 -t 600 -s 2 2>&1 | ./ParseResults.sh
-./AllRepair.sh Benchmarks/Qlose/loops_with_two -m 1 -u 1 -t 600 -s 2 2>&1 | ./ParseResults.sh
-./AllRepair.sh Benchmarks/Qlose/loops_with_two -m 1 -u 2 -t 600 -s 2 2>&1 | ./ParseResults.sh
-./AllRepair.sh Benchmarks/Qlose/loops_with_two -m 1 -u 5 -t 600 -s 2 2>&1 | ./ParseResults.sh
-./AllRepair.sh Benchmarks/Qlose/loops_with_two -m 1 -u 10 -t 600 -s 2 2>&1 | ./ParseResults.sh
-./AllRepair.sh Benchmarks/Qlose/loops_with_two -m 1 -u 15 -t 600 -s 2 2>&1 | ./ParseResults.sh
+./AllRepair.sh Benchmarks/Qlose/loops -m 1 -u 1 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/QLOSE
+./AllRepair.sh Benchmarks/Qlose/loops -m 1 -u 2 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/QLOSE
+./AllRepair.sh Benchmarks/Qlose/loops -m 1 -u 5 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/QLOSE
+./AllRepair.sh Benchmarks/Qlose/loops -m 1 -u 10 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/QLOSE
+./AllRepair.sh Benchmarks/Qlose/loops -m 1 -u 15 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/QLOSE
+./AllRepair.sh Benchmarks/Qlose/loops_with_two -m 1 -u 1 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/QLOSE
+./AllRepair.sh Benchmarks/Qlose/loops_with_two -m 1 -u 2 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/QLOSE
+./AllRepair.sh Benchmarks/Qlose/loops_with_two -m 1 -u 5 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/QLOSE
+./AllRepair.sh Benchmarks/Qlose/loops_with_two -m 1 -u 10 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/QLOSE
+./AllRepair.sh Benchmarks/Qlose/loops_with_two -m 1 -u 15 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/QLOSE
 # mutation level 2, increasing bounds 
-./AllRepair.sh Benchmarks/Qlose/loops -m 2 -u 1 -t 600 -s 2 2>&1 | ./ParseResults.sh
-./AllRepair.sh Benchmarks/Qlose/loops -m 2 -u 2 -t 600 -s 2 2>&1 | ./ParseResults.sh
-./AllRepair.sh Benchmarks/Qlose/loops -m 2 -u 5 -t 600 -s 2 2>&1 | ./ParseResults.sh
-./AllRepair.sh Benchmarks/Qlose/loops -m 2 -u 10 -t 600 -s 2 2>&1 | ./ParseResults.sh
-./AllRepair.sh Benchmarks/Qlose/loops -m 2 -u 15 -t 600 -s 2 2>&1 | ./ParseResults.sh
-./AllRepair.sh Benchmarks/Qlose/loops_with_two -m 2 -u 1 -t 600 -s 2 2>&1 | ./ParseResults.sh
-./AllRepair.sh Benchmarks/Qlose/loops_with_two -m 2 -u 2 -t 600 -s 2 2>&1 | ./ParseResults.sh
-./AllRepair.sh Benchmarks/Qlose/loops_with_two -m 2 -u 5 -t 600 -s 2 2>&1 | ./ParseResults.sh
-./AllRepair.sh Benchmarks/Qlose/loops_with_two -m 2 -u 10 -t 600 -s 2 2>&1 | ./ParseResults.sh
-./AllRepair.sh Benchmarks/Qlose/loops_with_two -m 2 -u 15 -t 600 -s 2 2>&1 | ./ParseResults.sh
+./AllRepair.sh Benchmarks/Qlose/loops -m 2 -u 1 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/QLOSE
+./AllRepair.sh Benchmarks/Qlose/loops -m 2 -u 2 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/QLOSE
+./AllRepair.sh Benchmarks/Qlose/loops -m 2 -u 5 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/QLOSE
+./AllRepair.sh Benchmarks/Qlose/loops -m 2 -u 10 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/QLOSE
+./AllRepair.sh Benchmarks/Qlose/loops -m 2 -u 15 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/QLOSE
+./AllRepair.sh Benchmarks/Qlose/loops_with_two -m 2 -u 1 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/QLOSE
+./AllRepair.sh Benchmarks/Qlose/loops_with_two -m 2 -u 2 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/QLOSE
+./AllRepair.sh Benchmarks/Qlose/loops_with_two -m 2 -u 5 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/QLOSE
+./AllRepair.sh Benchmarks/Qlose/loops_with_two -m 2 -u 10 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/QLOSE
+./AllRepair.sh Benchmarks/Qlose/loops_with_two -m 2 -u 15 -t 600 -s 2 2>&1 | ./ParseResults.sh AllRepairResults/QLOSE
